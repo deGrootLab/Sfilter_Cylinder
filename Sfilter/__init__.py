684d9916c8cb4a2d52d37f462f9f098a9620beb3
Chenggong <chenggong.hui@mpinat.mpg.de>
2023-07-20 11:32:56 +0200

    New version information is now added

--- Sfilter/__init__.py
+++ Sfilter/__init__.py
@@ -1,5 +1,7 @@
-from .Sfilter import Sfilter
+from .sfilter import sfilter
 from .util.output_wrapper import Perm_event_output
 from .util.output_wrapper import Cylinder_output
 from .util.output_wrapper import read_k_cylinder
 from .util import MSM
+
+__version__ = '0.5.1'
--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
 from setuptools import setup, find_packages
+import codecs
+import os.path
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 setup(
     name='Sfilter',
-    version='0.4.0',
+    version=get_version("Sfilter/__init__.py"),
     description='This is a tool for basic analysis in Potassium Channel MD simulation.',
     author='Chenggong Hui',
     author_email='chenggong.hui@mpinat.mpg.de',
