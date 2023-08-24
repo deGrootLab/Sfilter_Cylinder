c1078dc7d5dcbfce9c164a311cd3c9fa2d7c0175
Chenggong <chenggong.hui@mpinat.mpg.de>
2023-08-24 15:16:44 +0200

    add Langevin package

--- Sfilter/__init__.py
+++ Sfilter/__init__.py
@@ -3,5 +3,6 @@ from .util.output_wrapper import Perm_event_output
 from .util.output_wrapper import Cylinder_output
 from .util.output_wrapper import read_k_cylinder
 from .util import MSM
+from .util import Langevin
 
 __version__ = '0.5.6_dev'
