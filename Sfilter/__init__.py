c1869ed6f09747918ddd3d62b37ae6fc9c01df84
Chenggong <chenggong.hui@mpinat.mpg.de>
2023-09-08 16:43:23 +0200

    new module kinetics added

--- Sfilter/__init__.py
+++ Sfilter/__init__.py
@@ -3,6 +3,7 @@ from .util.output_wrapper import Perm_event_output
 from .util.output_wrapper import Cylinder_output
 from .util.output_wrapper import read_k_cylinder
 from .util import MSM
+from .util import kinetics
 from .util import Langevin
 
 __version__ = '0.6.1_dev'
